3b0d46f880c403b6d93548e1d58caf660c66bb22
Suchark <Suchanuch28@gmail.com>
2024-12-17 16:02:36 +0700

    update DeviceDetailPage

--- src/pages/DeviceDetail.tsx
+++ src/pages/DeviceDetail.tsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
-import { Box, Typography, Divider } from "@mui/material";
+import { Box, Typography, Divider, Button } from "@mui/material";
 import { useLocation } from "react-router-dom";
 import { IDevice } from "../interface/IDevice";
 import { getDeviceData } from "../api/DeviceDetailApi";
 import DeviceDetailComponent from "../components/devicesComponents/deviceDetail/DeviceDetailComponent";  // Updated import
 import DeviceInterfaceComponent from "../components/devicesComponents/deviceDetail/DeviceInterfaceComponent";
+import useWindowSize from "../hooks/useWindowSize";
 
 const DeviceDetailPage = () => {
+  const windowSize = useWindowSize();
   const location = useLocation();
   const [deviceData, setDeviceData] = useState<IDevice | null>(
     location.state?.device || null
@@ -52,19 +54,125 @@ const DeviceDetailPage = () => {
   }
 
   return (
-    <Box sx={{ marginTop: 2, padding: 4 }}>
-      <Typography variant="h4" component="h1" fontWeight={600} color={"#242D5D"}>
-        Device Details
-      </Typography>
+    <>
+      {windowSize.width > 600 && (
+        <Box
+          sx={{
+            width: 1,
+            display: "flex",
+            justifyContent: "flex-start",
+            marginTop: 5,
+            marginBottom: 3,
+          }}
+        >
+          <Typography
+            variant="h4"
+            component="h1"
+            fontWeight={600}
+            color={"#242D5D"}
+          >
+            DEVICE'S DETAIL
+          </Typography>
+        </Box>
+      )}
 
-      {/* Device Info Component */}
-      <DeviceDetailComponent deviceData={deviceData} />
+      <Box
+        sx={{
+          width: 1,
+          marginTop: 2,
+          height: "auto",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Box
+          sx={{
+            backgroundColor: "#FFFFFB",
+            flex: 1,
+            display: "flex",
+            borderRadius: 8,
+            flexDirection: "column",
+            alignItems: "center",
+            minHeight: "fit-content",
+            marginBottom: 5,
+            height: 1,
+            py: 3,
+          }}
+        >
+          <DeviceDetailComponent deviceData={deviceData} />
+        </Box>
 
-      <Divider sx={{ marginTop: 3, marginBottom: 3 }} />
+        <Divider sx={{ marginTop: 3, marginBottom: 3 }} />
 
-      {/* Device Items Component */}
-      <DeviceInterfaceComponent items={deviceData.items} />
-    </Box>
+        <Box
+          sx={{
+            width: 1,
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginBottom: 3,
+          }}
+        >
+          <Typography
+            variant="h4"
+            component="h1"
+            fontWeight={600}
+            color={"#242D5D"}
+          >
+            INTERFACE
+          </Typography>
+          <Button
+            type="submit"
+            sx={{
+              color: "#FFFFFB",
+              backgroundColor: "#F25A28",
+              fontSize: "1rem",
+              fontWeight: 600,
+              borderRadius: "70px",
+              width: "5.5rem",
+              height: "2.5rem",
+              "&:focus": {
+                outline: "none",
+                color: "#FFFFFB",
+              },
+              "&:hover": {
+                backgroundColor: "#F37E58",
+              },
+            }}
+          >
+            Graph
+          </Button>
+        </Box>
+
+        <Box
+          sx={{
+            backgroundColor: "#FFFFFB",
+            flex: 1,
+            display: "flex",
+            borderRadius: 8,
+            flexDirection: "column",
+            justifyContent: windowSize.width >= 1100 ? "center" : "start",
+            alignItems: "center",
+            minHeight: "fit-content",
+            marginBottom: 5,
+            py: 3,
+          }}
+        >
+          {windowSize.width < 1100 && (
+            <Typography
+              align="center"
+              sx={{
+                mt: "6rem",
+                mb: "2rem",
+              }}
+            >
+              {/* You can add any placeholder text here if necessary */}
+            </Typography>
+          )}
+          {deviceData && <DeviceInterfaceComponent items={deviceData.items} />}
+        </Box>
+      </Box>
+    </>
   );
 };
 
